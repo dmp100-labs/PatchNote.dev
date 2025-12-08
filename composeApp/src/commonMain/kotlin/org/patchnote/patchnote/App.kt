0e7122686b1e3cad216e30964358197bcc10e40c
Kyu hyunSung <dmp100@naver.com>
2025-12-08 21:34:22 +0900

    refactor : add comments for test

--- composeApp/src/commonMain/kotlin/org/patchnote/patchnote/App.kt
+++ composeApp/src/commonMain/kotlin/org/patchnote/patchnote/App.kt
@@ -18,7 +18,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import patchnote11.composeapp.generated.resources.Res
 import patchnote11.composeapp.generated.resources.compose_multiplatform
-// Test Coderabbit
+// Test Coderabbit22
 @Composable
 @Preview
 fun App() {
